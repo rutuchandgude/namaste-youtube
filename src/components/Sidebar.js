dcc27736a9dc6c499e10e327a455ff4119b07e5f
Rutuja Chandgude <rutuchandgude@gmail.com>
2024-03-22 14:59:16 +0530

    Youtube Webpage Loading

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Sidebar = ()=>{
     const isMenuOpen = useSelector(store => store.app.isMenuOpen);
@@ -7,7 +8,9 @@ const Sidebar = ()=>{
 return (
     <div className="p-5 shadow-lg w-48">
          <ul>
-            <li>Music</li>
+           <li>
+            <Link to="/">Home</Link>
+            </li>
             <li>Sports</li>
             <li>Gaming</li>
             <li>Movies</li>
